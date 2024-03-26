9a1303b1270067f2a9c58d344a94ea10f418d990
11valen12 <103825663+11valen12@users.noreply.github.com>
2024-03-27 00:56:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ from telebot import types
 import re
 
 # Замените 'YOUR_API_TOKEN' на ваш собственный API токен
-bot = telebot.TeleBot('7135892639:AAE81aR0pS20ieyUF8N_D4AbqGRt7OAM7KU')
+bot = telebot.TeleBot('YOUR_API_TOKEN')
 
 # Глобальная переменная для хранения информации о студентах
-students_data = []
-
-# Глобальная переменная для хранения информации о последних отметках
-last_attendance = {}
+students_data = {}
 
 
 # Обработчик команды /start и начала процесса отметки
@@ -40,7 +37,7 @@ def handle_attendance(message):
 def process_group_callback(call):
     group = call.data
     chat_id = call.message.chat.id
-    students_data.append({'group': group, 'chat_id': chat_id})
+    students_data[chat_id] = {'group': group}
     bot.send_message(chat_id, "Введите ваше ФИО:")
     bot.register_next_step_handler(call.message, process_name)
 
@@ -53,8 +50,7 @@ def process_name(message):
         bot.send_message(chat_id, "Пожалуйста, введите ваше ФИО в формате Иванов Иван.")
         bot.register_next_step_handler(message, process_name)
         return
-    group = next(item['group'] for item in students_data if item['chat_id'] == chat_id)
-    last_attendance[chat_id] = {'group': group, 'name': name, 'username': message.from_user.username}
+    students_data[chat_id]['name'] = name
     bot.send_message(chat_id, "Введите причину вашего отсутствия:")
     bot.register_next_step_handler(message, process_reason)
 
@@ -63,7 +59,7 @@ def process_name(message):
 def process_reason(message):
     chat_id = message.chat.id
     reason = message.text.strip()
-    last_attendance[chat_id]['reason'] = reason
+    students_data[chat_id]['reason'] = reason
     bot.send_message(chat_id, "При необходимости загрузите фотографию документа:")
     bot.register_next_step_handler(message, process_photo)
 
@@ -74,50 +70,23 @@ def process_photo(message):
     photo = None
     if message.photo:
         photo = message.photo[-1].file_id
-    last_attendance[chat_id]['photo'] = photo
-    send_attendance(chat_id)
+    students_data[chat_id]['photo'] = photo
+    send_attendance(chat_id, message.from_user.username)
 
 
 # Отправка информации об отсутствии в общий чат
-def send_attendance(chat_id):
-    group = last_attendance[chat_id]['group']
-    name = last_attendance[chat_id]['name']
-    reason = last_attendance[chat_id]['reason']
-    username = last_attendance[chat_id]['username']
-    photo = last_attendance[chat_id]['photo']
-    attendance_info = f"Группа: {group}\nФИО: {name}\nПричина: {reason}\n@{username}"
+def send_attendance(chat_id, username):
+    group = students_data[chat_id]['group']
+    name = students_data[chat_id]['name']
+    reason = students_data[chat_id]['reason']
+    photo = students_data[chat_id]['photo']
+    attendance_info = f"Username: @{username}\nГруппа: {group}\nФИО: {name}\nПричина: {reason}"
     if photo:
         bot.send_photo(-1002061475233, photo, caption=attendance_info)
     else:
+        attendance_info += "\nФотография документа не загружена."
         bot.send_message(-1002061475233, attendance_info)
     bot.send_message(chat_id, "Ваша отметка об отсутствии отправлена.")
-
-
-# Обработчик команды для преподавателя получить последние 5 отметок
-@bot.message_handler(commands=['last'])
-def send_last_attendance(message):
-    chat_id = message.chat.id
-    markup = types.ReplyKeyboardMarkup(row_width=2)
-    group_buttons = [types.KeyboardButton(text=group) for group in ["Дв311", "1321", "1212", "П21", "3211"]]
-    markup.add(*group_buttons)
-    bot.send_message(chat_id, "Выберите группу:", reply_markup=markup)
-
-
-# Обработка выбора группы при запросе последних отметок
-@bot.message_handler(func=lambda message: message.text in ["Дв311", "1321", "1212", "П21", "3211"])
-def handle_last_attendance_group(message):
-    group = message.text
-    chat_id = message.chat.id
-    last_attendance_for_group = [attendance for attendance in last_attendance.values() if attendance['group'] == group][-5:]
-    if not last_attendance_for_group:
-        bot.send_message(chat_id, f"Нет данных об отсутствии студентов в группе {group}.")
-        return
-    for attendance in last_attendance_for_group:
-        attendance_info = f"Группа: {attendance['group']}\nФИО: {attendance['name']}\nПричина: {attendance['reason']}\n@{attendance['username']}"
-        if attendance['photo']:
-            bot.send_photo(chat_id, attendance['photo'], caption=attendance_info)
-        else:
-            bot.send_message(chat_id, attendance_info)
 
 
 # Запуск бота
